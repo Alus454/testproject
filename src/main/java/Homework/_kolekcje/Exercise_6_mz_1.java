dd49a57bb739a72b9e1c19d7ff43f03f5d8f2393
Alus454 <alukard454@gmail.com>
2020-06-23 12:25:17 +0200

    zadanie 6

--- src/main/java/Homework/_kolekcje/Exercise_6_mz_1.java
+++ src/main/java/Homework/_kolekcje/Exercise_6_mz_1.java
@@ -1,4 +1,27 @@
 package Homework._kolekcje;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Exercise_6_mz_1 {
+
+    public static void addQueue(int maxQueue){
+        Queue queue = new PriorityQueue();
+        Random random = new Random();
+        for (int i = 0;i< maxQueue;i++){
+            queue.add((random.nextInt(100)+1));
+
+        }
+        System.out.println(queue);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int maxQueue = scanner.nextInt();
+        addQueue(maxQueue);
+    }
+
 }
